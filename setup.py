a46b8743ba5311e9c29fcad81b963f5030669eb9
saritakarki <saritakarki00053@gmail.com>
2024-08-08 10:22:59 +0545

    update setup.py

--- setup.py
+++ setup.py
@@ -5,8 +5,8 @@ setup(
     version='0.1.0',
     packages=find_packages(),
     install_requires=[
-        'PyMuPDF',
-        'unstructured',
+        'PyMuPDF==1.23.26',
+        'unstructured~=0.14.2',
         'unstructured[pdf]'
     ],
 )
