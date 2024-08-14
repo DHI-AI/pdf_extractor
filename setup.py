66bae456b13f63da838781852a21a9520572dcd2
saritakarki <saritakarki00053@gmail.com>
2024-08-14 14:13:54 +0545

    Add table explanation for pdf

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
 from setuptools import setup, find_packages
 
+# Function to parse requirements.txt
+def parse_requirements(filename):
+    with open(filename, "r") as file:
+        return file.read().splitlines()
+
 setup(
     name='pdf_extractor',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[
-        'PyMuPDF==1.23.26',
-        'unstructured~=0.14.2',
-        'unstructured[pdf]'
-    ],
+    install_requires=parse_requirements("requirements.txt"),
 )
